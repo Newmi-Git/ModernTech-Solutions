2cc137c794e969664f95cbe5840d07e23c5ea02d
Newmi-Git <yaghyaabdul1006@gmail.com>
2026-08-14 07:40:20 +0200

    fixed cvalidatior

--- backend/validators/leaveRequestValidator.js
+++ backend/validators/leaveRequestValidator.js
@@ -10,7 +10,7 @@ const validateLeaveRequest = [
 function handleValidation(req, res, next) {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ success: false, errors: errors.array() });
+    return res.status(400).json({ success: false, errors: errors.array()});
   }
   next();
 }
